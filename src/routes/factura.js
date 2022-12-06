3c8b71fa26f120618d69e46642a5446fc6e9f44c
Julieth MF <juliethmf1@gmail.com>
2022-12-05 22:27:24 -0500

    routes factura

---
+++ src/routes/factura.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import factura from '@controllers/factura'
+
+const router = new express.Router()
+
+router.get('/:id', factura.consutlarUna)
+router.get('/:sucid/:texto/:tipofac', factura.consutlar)
+router.post('/', factura.insertar)
+router.delete('/:id', factura.eliminar)
+router.put('/:id', factura.actualizar)
+router.put('/asentar/:id/:fecha', factura.asentar)
+router.put('/asentar/compra/:id/:fecha', factura.asentarcompra)
+router.put('/reversar/:id', factura.reversar)
+
+export default router
